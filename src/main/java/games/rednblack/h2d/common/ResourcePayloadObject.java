c2ffd96382b82d42e283716546cb4b184c9b531e
qlang <1224076583@qq.com>
2021-06-21 22:26:19 +0800

    add index region support.

--- src/main/java/games/rednblack/h2d/common/ResourcePayloadObject.java
+++ src/main/java/games/rednblack/h2d/common/ResourcePayloadObject.java
@@ -18,11 +18,12 @@
 
 package games.rednblack.h2d.common;
 
-public class ResourcePayloadObject {
+public class ResourcePayloadObject<T> {
     public String name;
     public float xOffset;
     public float yOffset;
     public String className;
+    public T extra;
 
     public ResourcePayloadObject() {
     }
--- src/main/java/games/rednblack/h2d/common/factory/IFactory.java
+++ src/main/java/games/rednblack/h2d/common/factory/IFactory.java
@@ -3,13 +3,26 @@ package games.rednblack.h2d.common.factory;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.math.Vector2;
 
+import games.rednblack.h2d.common.ResourcePayloadObject;
+
 public interface IFactory {
     Entity getCreatedEntity();
 
-    boolean createSimpleImage(String regionName, Vector2 position);
-    boolean createAtlasImage(String regionName, Vector2 position);
-    boolean createSpriteAnimation(String animationName, Vector2 position);
-    boolean createSpineAnimation(String animationName, Vector2 position);
-    boolean createSpriterAnimation(String animationName, Vector2 position);
-    boolean createItemFromLibrary(String libraryName, Vector2 position);
+    boolean createSimpleImage(String name, int index, Vector2 position);
+
+    boolean createSimpleImage(ResourcePayloadObject payload, Vector2 position);
+
+    boolean createAtlasImage(ResourcePayloadObject payload, Vector2 position);
+
+    boolean createSpriteAnimation(String name, Vector2 position);
+
+    boolean createSpriteAnimation(ResourcePayloadObject payload, Vector2 position);
+
+    boolean createSpineAnimation(String name, Vector2 position);
+
+    boolean createSpineAnimation(ResourcePayloadObject payload, Vector2 position);
+
+    boolean createSpriterAnimation(ResourcePayloadObject payload, Vector2 position);
+
+    boolean createItemFromLibrary(ResourcePayloadObject payload, Vector2 position);
 }
